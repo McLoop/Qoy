68d6ebd3a9dd8b0f6e567aa5484c71d26fdaec4a
Brian Lopez ledezma <brianlolepez360@gmail.com>
2021-09-01 21:31:05 -0400

    formateo con header y footer limpios, modo oscuro al 50%, fondos en modo oscuro al 100, solo falta el color de fuente

--- resources/views/inicio.blade.php
+++ resources/views/inicio.blade.php
@@ -9,17 +9,11 @@
     <link rel="icon" href="{{ asset('images/favicon.ico') }}" type="image/x-icon">
     <!-- Stylesheets-->
     <link rel="stylesheet" type="text/css" href="//fonts.googleapis.com/css?family=Roboto:100,300,300i,400,500,600,700,900%7CRaleway:500">
-    <link
-      rel="stylesheet"
-      href="https://use.fontawesome.com/releases/v5.13.0/css/all.css"
-      integrity="sha384-Bfad6CLCknfcloXFOyFnlgtENryhrpZCe29RTifKEixXQZ38WheV+i/6YWSzkz3V"
-      crossorigin="anonymous"
-    />
     <link rel="stylesheet" href="{{ asset('css/bootstrap.css') }}">
     <link rel="stylesheet" href="{{ asset('css/fonts.css') }}">
     <link rel="stylesheet" href="{{ asset('css/style.css') }}">
   </head>
-  <body class="">
+  <body>
     <div class="lds-ring">
       <div></div>
       <div></div>
@@ -40,7 +34,7 @@
                   <button class="rd-navbar-toggle" data-rd-navbar-toggle=".rd-navbar-nav-wrap"><span></span></button>
                   <!-- RD Navbar Brand-->
                   <div class="rd-navbar-brand">
-                    <a class="brand" href="index.html"><img class="brand-logo-dark" src="{{ asset('images/logo-198x66.png') }}" alt="" width="198" height="66"/></a>
+                    <a class="brand" href="index.html"><img class="brand-logo-dark" src="{{ asset('images/logo.png') }}" alt="" width="198" height="66"/></a>
                   </div>
                 </div>
                 <div class="rd-navbar-right rd-navbar-nav-wrap">
@@ -48,24 +42,34 @@
                     <ul class="rd-navbar-contacts-2">
                       <li>
                         <div class="unit unit-spacing-xs">
-                          <div class="unit-left"><i id="icon-y" class="fas fa-info-circle fa-lg"></i></div>
-                          <div class="unit-body"><a href="#">El sitio donde puede encontrar todo ¡GRATIS!</a></div>
+                          <div class="unit-left"><span class="icon mdi mdi-home"></span></div>
+                          <div class="unit-body"><a class="phone" href="tel:#">+1 718-999-3939</a></div>
+                        </div>
+                      </li>
+                      <li>
+                        <div class="unit unit-spacing-xs">
+                          <div class="unit-left"><span class="icon mdi mdi-map-marker"></span></div>
+                          <div class="unit-body"><a class="address" href="#">514 S. Magnolia St. Orlando, FL 32806</a></div>
                         </div>
                       </li>
                     </ul>
                     <ul class="list-share-2">
-                      <li><a id="btn-rrss" class="icon mdi mdi-facebook" href="#"></a></li>
-                      <li><a id="btn-rrss" class="icon mdi mdi-instagram" href="#"></a></li>
+                      <li><a class="icon mdi mdi-facebook" href="#"></a></li>
+                      <li><a class="icon mdi mdi-twitter" href="#"></a></li>
+                      <li><a class="icon mdi mdi-instagram" href="#"></a></li>
+                      <li><a class="icon mdi mdi-google-plus" href="#"></a></li>
                     </ul>
                   </div>
                   <div class="rd-navbar-main">
                     <!-- RD Navbar Nav-->
                     <ul class="rd-navbar-nav">
-                      <li class="rd-nav-item active"><a class="rd-nav-link" href="#info1">¿Quienes somos?</a>
+                      <li class="rd-nav-item active"><a class="rd-nav-link" href="index.html">Home</a>
                       </li>
-                      <li class="rd-nav-item"><a class="rd-nav-link" href="#info2">¿Qué hacemos?</a>
+                      <li class="rd-nav-item"><a class="rd-nav-link" href="about-us.html">About us</a>
                       </li>
-                      <li class="rd-nav-item"><a class="rd-nav-link" href="#info3">Unete</a>
+                      <li class="rd-nav-item"><a class="rd-nav-link" href="typography.html">Typography</a>
+                      </li>
+                      <li class="rd-nav-item"><a class="rd-nav-link" href="contacts.html">Contacts</a>
                       </li>
                     </ul>
                   </div>
@@ -137,30 +141,63 @@
       </header>
 
       <!-- inicio body -->
-      <section id="info1">
-        <h2>hasalksf</h2>
-      </section>
-      <section id="info2">
-      </section>
-      <section id="info3">
-      </section>
+     
      <!--fin del body-->
 
+      <!-- Bottom Banner--><a class="section section-banner" href="https://www.templatemonster.com/intense-multipurpose-html-template.html" style="background-image: url(images/banner/background-03-1920x310.jpg); background-image: -webkit-image-set( url(images/banner/background-03-1920x310.jpg) 1x, url(images/banner/background-03-3840x620.jpg) 2x )"><img src="images/banner/foreground-03-1600x310.png" srcset="images/banner/foreground-03-1600x310.png 1x, images/banner/foreground-03-3200x620.png 2x" alt="" width="1600" height="310"></a>
       <!-- Page Footer-->
       <footer class="section footer-modern context-dark footer-modern-2">
-        <!--<div class="footer-modern-line">
+        <div class="footer-modern-line">
           <div class="container">
             <div class="row row-50">
-              
+              <div class="col-md-6 col-lg-4">
+                <h5 class="footer-modern-title oh-desktop"><span class="d-inline-block wow slideInLeft">What We Offer</span></h5>
+                <ul class="footer-modern-list d-inline-block d-sm-block wow fadeInUp">
+                  <li><a href="#">Pizzas</a></li>
+                  <li><a href="#">Burgers</a></li>
+                  <li><a href="#">Salads</a></li>
+                  <li><a href="#">Drinks</a></li>
+                  <li><a href="#">Seafood</a></li>
+                  <li><a href="#">Drinks</a></li>
+                </ul>
+              </div>
+              <div class="col-md-6 col-lg-4 col-xl-3">
+                <h5 class="footer-modern-title oh-desktop"><span class="d-inline-block wow slideInLeft">Information</span></h5>
+                <ul class="footer-modern-list d-inline-block d-sm-block wow fadeInUp">
+                  <li><a href="about-us.html">About us</a></li>
+                  <li><a href="#">Latest News</a></li>
+                  <li><a href="#">Our Menu</a></li>
+                  <li><a href="#">FAQ</a></li>
+                  <li><a href="#">Shop</a></li>
+                  <li><a href="contacts.html">Contact Us</a></li>
+                </ul>
+              </div>
+              <div class="col-lg-4 col-xl-5">
+                <h5 class="footer-modern-title oh-desktop"><span class="d-inline-block wow slideInLeft">Newsletter</span></h5>
+                <p class="wow fadeInRight">Sign up today for the latest news and updates.</p>
+                <!-- RD Mailform-->
+                <form class="rd-form rd-mailform rd-form-inline rd-form-inline-sm oh-desktop" data-form-output="form-output-global" data-form-type="subscribe" method="post" action="bat/rd-mailform.php">
+                  <div class="form-wrap wow slideInUp">
+                    <input class="form-input" id="subscribe-form-2-email" type="email" name="email" data-constraints="@Email @Required"/>
+                    <label class="form-label" for="subscribe-form-2-email">Enter your E-mail</label>
+                  </div>
+                  <div class="form-button form-button-2 wow slideInRight">
+                    <button class="button button-sm button-icon-3 button-primary button-winona" type="submit"><span class="d-none d-xl-inline-block">Subscribe</span><span class="icon mdi mdi-telegram d-xl-none"></span></button>
+                  </div>
+                </form>
+              </div>
             </div>
           </div>
-        </div>-->
+        </div>
         <div class="footer-modern-line-2">
           <div class="container">
             <div class="row row-30 align-items-center">
               <div class="col-sm-6 col-md-7 col-lg-4 col-xl-4">
                 <div class="row row-30 align-items-center text-lg-center">
-                  <div class="col-md-7 col-xl-6"><a class="brand" href="index.html"><img src="{{ asset('images/logo-198x66.png') }}" alt="" width="198" height="66"/></a></div>
+                  <div class="col-md-7 col-xl-6"><a class="brand" href="index.html"><img src="images/logo-inverse-198x66.png" alt="" width="198" height="66"/></a></div>
+                  <div class="col-md-5 col-xl-6">
+                    <div class="iso-1"><span><img src="images/like-icon-58x25.png" alt="" width="58" height="25"/></span><span class="iso-1-big">9.4k</span></div>
+                  </div>
                 </div>
               </div>
               <div class="col-sm-6 col-md-12 col-lg-8 col-xl-8 oh-desktop">
@@ -168,19 +205,21 @@
                   <div class="footer-modern-contacts wow slideInUp">
                     <div class="unit unit-spacing-sm align-items-center">
                       <div class="unit-left"><span class="icon icon-24 mdi mdi-phone"></span></div>
-                      <div class="unit-body"><a class="phone" href="tel:#">Comunícate con nosotros</a></div>
+                      <div class="unit-body"><a class="phone" href="tel:#">+1 718-999-3939</a></div>
                     </div>
                   </div>
                   <div class="footer-modern-contacts wow slideInDown">
                     <div class="unit unit-spacing-sm align-items-center">
                       <div class="unit-left"><span class="icon mdi mdi-email"></span></div>
-                      <div class="unit-body"><a class="mail" href="mailto:qoybolivia@gmail.com">qoybolivia@gmail.com</a></div>
+                      <div class="unit-body"><a class="mail" href="mailto:#">info@demolink.org</a></div>
                     </div>
                   </div>
                   <div class="wow slideInRight">
                     <ul class="list-inline footer-social-list footer-social-list-2 footer-social-list-3">
                       <li><a class="icon mdi mdi-facebook" href="#"></a></li>
+                      <li><a class="icon mdi mdi-twitter" href="#"></a></li>
                       <li><a class="icon mdi mdi-instagram" href="#"></a></li>
+                      <li><a class="icon mdi mdi-google-plus" href="#"></a></li>
                     </ul>
                   </div>
                 </div>
